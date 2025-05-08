d1fa605f699fb56ece33c96f4287d5fae5bf818b
kuyugama <mail.kuyugama@gmail.com>
2025-05-08 12:27:14 +0300

    Improve dependencies using FunDI's parameter awareness of dependencies.py

--- fastapi_websocket/__init__.py
+++ fastapi_websocket/__init__.py
@@ -1,4 +1,4 @@
 from .domain import WebsocketDomain
 from .error import BaseRequestError, RequestError
 from .types import SendEvent, SendError, create_event, create_error_event
-from .dependencies import from_model, from_adapter, from_header, from_cookie, from_query, from_path
+from .dependencies import from_model, from_adapter, from_header, from_cookie, from_query, from_path, require_body
--- fastapi_websocket/dependencies.py
+++ fastapi_websocket/dependencies.py
@@ -1,6 +1,6 @@
 from typing import TypeVar, Any
-from fundi import from_, FromType
 from pydantic import BaseModel, TypeAdapter
+from fundi import from_, FromType, Parameter
 from starlette.datastructures import Headers, QueryParams
 
 Tm = TypeVar("Tm", bound=BaseModel)
@@ -21,37 +21,80 @@ def from_adapter(adapter: TypeAdapter[Tt]) -> Tt:
     return from_(validator)
 
 
-def from_header(name: str, type_: type[Tt]) -> Tt:
-    adapter = TypeAdapter(type_)
+def require_body(request_data: Any, param: FromType[Parameter]) -> Any:
+    if isinstance(param.annotation, BaseModel):
+        return param.annotation.model_validate(request_data)
+    else:
+        return TypeAdapter(param.annotation).validate_python(request_data)
 
-    def validator(headers: FromType[Headers]) -> Tt:
+
+def from_header(name: str = ..., type_: type[Tt] = ...) -> Tt:
+    name = None if name is Ellipsis else name
+    adapter = None if type_ is Ellipsis else TypeAdapter(type_)
+
+    def validator(headers: FromType[Headers], param: FromType[Parameter]) -> Tt:
+        nonlocal adapter, name
+
+        if adapter is None:
+            adapter = TypeAdapter(param.annotation)
+
+        if name is None:
+            name = param.name
+
         return adapter.validate_python(headers.get(name))
 
     return from_(validator)
 
 
 def from_cookie(name: str, type_: type[Tt]) -> Tt:
-    adapter = TypeAdapter(type_)
+    name = None if name is Ellipsis else name
+    adapter = None if type_ is Ellipsis else TypeAdapter(type_)
 
-    def validator(cookies: dict[str, str]) -> Tt:
+    def validator(cookies: dict[str, str], param: FromType[Parameter]) -> Tt:
+        nonlocal adapter, name
+
+        if adapter is None:
+            adapter = TypeAdapter(param.annotation)
+
+        if name is None:
+            name = param.name
+
         return adapter.validate_python(cookies.get(name))
 
     return from_(validator)
 
 
 def from_query(name: str, type_: type[Tt]) -> Tt:
-    adapter = TypeAdapter(type_)
+    name = None if name is Ellipsis else name
+    adapter = None if type_ is Ellipsis else TypeAdapter(type_)
 
-    def validator(query_params: FromType[QueryParams]) -> Tt:
+    def validator(query_params: FromType[QueryParams], param: FromType[Parameter]) -> Tt:
+        nonlocal adapter, name
+
+        if adapter is None:
+            adapter = TypeAdapter(param.annotation)
+
+        if name is None:
+            name = param.name
+
         return adapter.validate_python(query_params.get(name))
 
     return from_(validator)
 
 
 def from_path(name: str, type_: type[Tt]) -> Tt:
-    adapter = TypeAdapter(type_)
+    name = None if name is Ellipsis else name
+    adapter = None if type_ is Ellipsis else TypeAdapter(type_)
 
-    def validator(path_params: dict[str, Any]) -> Tt:
+    def validator(path_params: dict[str, Any], param: FromType[Parameter]) -> Tt:
+        nonlocal adapter, name
+
+        if adapter is None:
+            adapter = TypeAdapter(param.annotation)
+
+        if name is None:
+            name = param.name
+
         return adapter.validate_python(path_params.get(name))
 
     return from_(validator)
